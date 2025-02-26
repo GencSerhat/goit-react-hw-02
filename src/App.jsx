f4fc5b5883f4105885c5c9918bb2588c5551fef8
Serhat GENÇ <gncsrht05@gmail.com>
2025-02-26 16:56:50 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Feedback from "./components/Feedback/feedback";
-import Description from "./components/description/Description";
+import Description from "./components/escription/Description";
 
 function App() {
   return (
